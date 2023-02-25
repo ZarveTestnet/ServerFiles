3b65e17a177de9f4772a5ee3061869175de2c790
Zarve8 <GZhitniy@icloud.com>
2023-02-25 07:59:24 -0800

    file storage added

---
+++ file_storage/files/file-server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3333;
+const cors = require('cors');
+
+
+app.use(cors())
+app.use(express.static(path.join(__dirname)));
+
+app.listen(port, () => {
+    console.log(`File Storage Server started on port: ${port}`);
+})
